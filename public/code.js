5e1616f1e60d57c9839d41da006a7a30b833b204
Your Name <you@example.com>
2013-03-08 00:46:22 +0200

    toinen portti

--- public/code.js
+++ public/code.js
@@ -22,7 +22,7 @@ var canvas,	// DOM
 **************************************************/
 function init() {
 	// Connect the server
-	socket = io.connect("yolo-adventure.azurewebsites.net", {port: 8000, transports: ["websocket"]});
+	socket = io.connect("yolo-adventure.azurewebsites.net", {port: 8080, transports: ["websocket"]});
 
 	// Init keys
 	keys = new Keys();
